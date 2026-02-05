5d9e08d2da759c0f4baf4a7b91f46d2d82da263b
Cosku Bas <cosku.bas@gmail.com>
2026-02-05 15:14:49 +0100

    update custom-kernel

--- files/scripts/initramfs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-KERNEL_VERSION="$(rpm -q --queryformat="%{evr}.%{arch}" kernel-cachyos-lto)"
-export DRACUT_NO_XATTR=1
-
-/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
-chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
